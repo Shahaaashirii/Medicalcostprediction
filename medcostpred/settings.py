bf45eb026dbed7af317983140900d9351a4eac18
Shahana Shirin <shahanashirin202@gmail.com>
2024-10-30 20:17:14 +0530

    Update project structure and configuration for Render deployment

--- medcostpred/settings.py
+++ medcostpred/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w^oz+o(c!otylh-1vj!&=$x3-s2g@e$ua42xwf70$%jkodf@**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medicalcostprediction.onrender.com']
 
 
 # Application definition
